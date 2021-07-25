7ce2f1aa0ede4fca2fb18da217ca03000db8f4aa
xovee <xovee@live.com>
2021-07-25 19:54:06 +0800

    add source code

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 # CANV: Co-Authorship Network Visualization
 
+![Example](asset/teaser.png)
+
+CANV is an interactive co-authorship network visualization tool based on [webweb](https://github.com/dblarremore/webweb). See an example above (my network). 
+
+## Dependency
+
+```shell
+pip install -r requirements.txt
+```
+
+## Usage
+
+You can create your co-authorship network with a few easy steps in no time.
+
+1. Open your [dblp](https://dblp.org) author page, download your bibliographic data, e.g.,  
+`xovee-xu.xml`  
+<img src="asset/download_data.png" alt="download data" width="250"/>
+
+2. Change variable `name` in `args` in `canv.py` to match the name of your data file, e.g.,  
+`'name': 'xovee-xu'`
+
+3. Run [canv.py](./canv.py), then you have `xovee-xu.html`, that's all! 
+
+## Option
+
+There are several options you can customize your CANV page.
+
+- `name`: your data file name
+- `display_name`: name displayed in webpage
+- `min_edge_weight`: filter out infrequent co-authors
+- `link_length`: you know what
+- `name_to_match`: show someone's name in default
+- `radius`: node size
+- `show_node_names`: show all co-author names in default or not
+- `show_percentage_names`: show top frequented co-author names (%), preferably 5-15% if you have a large number of co-authors; if this variable is not `0`, `frequent_co_authors` must be `None`
+- `frequent_co_authors`: a list contains co-author names, the webpage will display their names in default; if `frequent_co_authors` is not `None`, `show_percentage_names` must be `0`
+- `canvas_height`
+- `canvas_width`
+
+Check [canv.py](./canv.py) for more. Of course, you can directly edit [template.html](./template.html) to deeply customize your page. 
+
+PS: if you want to change the node color, search `d3.interpolateReds(webweb.scales.colors.scalar(x))` in [template.html](./template.html) and replace it within the color you like.
+
+## Acknowledgment
+
+Thanks to [webweb](https://github.com/dblarremore/webweb) and [dblp](https://dblp.org) team.
+
 ## LICENSE
 
 GPL-3.0 License
---
+++ canv.py
@@ -0,0 +1,197 @@
+import xmltodict
+import networkx as nx
+
+from webweb import Web
+import webweb
+
+
+def load_dblp_xml(path):
+    """
+    Load DBLP author metadata.
+    First, load author publication xml file. 
+    Download:
+        1. open your DBLP author page
+        2. the third icon right next your Name, hover and click the `XML` in the dropdown box
+        3. save your XML file
+    Then this function read your co-authorship records. 
+    """
+    with open(path) as f:
+        data = xmltodict.parse(f.read())
+
+    co_author_lst = list()
+
+    for paper in data['dblpperson']['r']:
+        author_lst = list()
+        # assumption: the first element of Ordered Dict 'paper' is paper type
+        paper_type = list(paper.keys())
+        if paper_type[0] in ['article', 'inproceedings']:
+            authors = paper[list(paper.keys())[0]]['author']
+        else:
+            continue
+
+        if len(authors) > 2:
+            for author in authors:
+                name = author['#text']
+                if name[-4:].isnumeric():
+                    name = name[:-5]
+                if '(-)' in name:
+                    name = name.replace('(-)', '')
+                author_lst.append(name)
+        else:
+            continue
+        co_author_lst.append(author_lst)
+
+    # len(co-author_lst): number of paper
+    # each of 'co-author_lst): list of co-authors of one paper
+    return co_author_lst
+
+
+def build_graph(co_author_lst, min_weight, frequent_co_authors=None, show_percentage_names=0):
+    # build graph
+    g = nx.Graph()
+
+    # add edges
+    for paper_author in co_author_lst:
+        for author in paper_author:
+            for another_author in paper_author:
+                if author != another_author:
+                    try:
+                        g[author][another_author]['weight'] += .5
+                    except KeyError:
+                        g.add_weighted_edges_from([[author, another_author, .5]])
+                    else:
+                        pass
+
+    # for an edge, if its weight (co-author times between two authors) less than 'min_weight', filter it out
+    edge_list = [[u, v, w['weight']] for (u, v, w) in g.edges(data=True) if w['weight'] >= min_weight]
+
+    nodes = set([u for (u, v, w) in edge_list]).union(set([v for (u, v, w) in edge_list]))
+
+    degrees = [d for (u, d) in g.degree(nbunch=list(nodes), weight='weight')]
+
+    # 
+    percentage_names = None 
+    if frequent_co_authors != None and show_percentage_names == 0:
+        percentage_names = frequent_co_authors
+    elif frequent_co_authors == None and show_percentage_names != 0:
+        min_percentage_weight = sorted(degrees, reverse=True)[:int(len(degrees) * (show_percentage_names / 100))][-1]
+        percentage_names = [u for (u, d) in g.degree(nbunch=list(nodes), weight='weight') if d >= min_percentage_weight]
+        print('The webpage will show following {} names in default:'.format(len(percentage_names)))
+        print('\t' + ', '.join(sorted(percentage_names, key=lambda name: name.split(' ')[-1])))
+    elif frequent_co_authors == None and show_percentage_names == 0:
+        percentage_names = None
+    else:
+        print("Whether 'frequent_co_authors' should be 'None' or 'show_percentage_names' should be zero.")
+        exit()
+        
+    return edge_list, percentage_names
+
+def read_and_write_html(path):
+    with open(path, 'r') as f:
+        html_file = f.read()
+        start_idx = html_file.index('<script type="text/javascript">var wwdata')
+        end_idx = html_file.index('</script>', start_idx) + 9
+        html_data = html_file[start_idx:end_idx]
+    
+    with open('./template.html', 'r') as f:
+        html_file = f.read()
+        start_idx = html_file.index('<script\n        type="text/javascript">var wwdata')
+        end_idx = html_file.index('</script>', start_idx) + 9
+        # print(html_file[start_idx:end_idx])
+        html_final = html_file.replace(html_file[start_idx:end_idx], html_data)
+    
+    with open(path, 'w') as f:
+        f.write(html_final)
+
+
+def main(name,
+         display_name='Xovee Xu',
+         min_edge_weight=1,
+         color_by='strength',
+         size_by='strength',
+         charge=150,
+         link_length=150,
+        #  color_map='Reds',
+         scale_link_opacity=1,
+         scale_link_width=1,
+         name_to_match='Xovee Xu',
+         radius=10,
+         show_node_names=0,
+         hide_menu=0,
+         show_legend=1,
+         frequent_co_authors=None,
+         show_percentage_names=0,
+         canvas_height = 700,
+         canvas_width = 1000,
+         ):
+
+    # load dblp data
+    co_author_lst = load_dblp_xml(path=name + '.xml')
+
+    # build graph
+    edge_list, percentage_names = build_graph(co_author_lst, 
+        min_edge_weight, frequent_co_authors=frequent_co_authors, 
+        show_percentage_names=show_percentage_names)
+
+    number_co_authors = len(set([u for (u, v, w) in edge_list] + [v for (u, v, w) in edge_list]))
+
+    # build webweb's web
+    web = Web(title=name_to_match)
+
+
+    web.networks.xovee(
+        adjacency=edge_list,
+    )
+
+    # display options
+    web.display.colorBy = color_by
+    web.display.sizeBy = size_by
+    web.display.charge = charge
+    web.display.linkLength = link_length
+    web.display.scaleLinkOpacity = scale_link_opacity
+    web.display.scaleLinkWidth = scale_link_width
+    web.display.nameToMatch = name_to_match
+    web.display.radius = radius
+    web.display.showNodeNames = show_node_names
+    web.display.hideMenu = hide_menu
+    web.display.showLegend = show_legend
+    web.display.frequentCoAuthors = percentage_names
+    web.display.h = canvas_height
+    web.display.w = canvas_width
+    web.display.numberCoAuthor = number_co_authors
+    web.display.displayName = display_name
+
+    # web.show()
+
+    web.save(name + '.html')
+
+    read_and_write_html(name + '.html')
+
+    # change display name
+
+
+if __name__ == '__main__':
+    args = {
+        'name': 'xovee-xu',
+        'display_name': 'Xovee Xu',
+        'min_edge_weight': 1,
+        'color_by': 'strength',  # 'degree' or 'strength'
+        'size_by': 'strength',  # 'degree' or 'strength'
+        'charge': 256,
+        'link_length': 200,
+        'scale_link_opacity': 1,
+        'scale_link_width': 1,
+        'name_to_match': "",
+        'radius': 15,
+        'show_node_names': 0,
+        'hide_menu': 0,
+        'show_legend': 0,
+        'show_percentage_names': 0,  # percentage; 0: don't show; perferably 5-15%;
+        'frequent_co_authors': None,
+        'canvas_height': 700,
+        'canvas_width': 1000,
+    }
+
+    main(**args)
+
+
